48ffe1da2a161b97b712064dc4bc6089652caacb
wigryz <tomasz.ligeza3@gmail.com>
2021-09-14 19:46:01 +0200

    Initialize Spring project
    issue #2

---
+++ src/test/java/com/example/cinemabackend/CinemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
