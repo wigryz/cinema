36571399748770c455b7c4a3955bb85b001e6aa9
wigryz <tomasz.ligeza3@gmail.com>
2021-09-15 18:51:55 +0200

    [#5] Configure unit tests and integration test plugin
    Unit tests now will be run in test stage of lifecycle,
    and integral tests will be run in failsafe:integration-test
    or in verify stage of lifecycle just after unit tests.
    
    Probably we should add separate lifecycle stage named
    integration-tests.

---
+++ src/main/java/com/it/cinemabackend/example/Example.java
@@ -0,0 +1,10 @@
+package com.it.cinemabackend.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Example {
+    public Integer add(Integer a, Integer b) {
+        return a + b;
+    }
+}
