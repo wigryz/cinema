4572a117c02240181d2572305655471f599103aa
wigryz <tomasz.ligeza3@gmail.com>
2021-09-16 22:26:19 +0200

    Save artifacts in CircleCI job
    Code coverage stats, unit and integration test results
    are now saved as artifacts in CircleCI builds.

--- src/test/java/com/it/cinemabackend/CinemaBackendApplicationIT.java
+++ src/test/java/com/it/cinemabackend/CinemaBackendApplicationIT.java
@@ -3,11 +3,13 @@ package com.it.cinemabackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @SpringBootTest
 class CinemaBackendApplicationIT {
 
     @Test
     void contextLoads() {
     }
-
 }
