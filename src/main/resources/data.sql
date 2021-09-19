75505d259784ad9083fb7f56b5e4b4b3a4689b58
wigryz <tomasz.ligeza3@gmail.com>
2021-09-19 13:33:43 +0200

    Configure h2 database for test scope

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO country (id, name)
+    VALUES (0, 'England');
+INSERT INTO movie (id, title, description, year_of_production, duration, rating, additional_info, country_id)
+    VALUES (0, 'The Batman', 'Simple description', 2001, 137, 7.2, 'Tickets only for adults', 0);
