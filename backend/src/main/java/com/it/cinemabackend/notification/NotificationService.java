82b91e694c3e6a426ba0846b3dc8fefed37ff277
wigryz <tomasz.ligeza3@gmail.com>
2021-10-23 11:05:16 +0200

    Add scheduled notification.

--- backend/src/main/java/com/it/cinemabackend/notification/NotificationService.java
+++ backend/src/main/java/com/it/cinemabackend/notification/NotificationService.java
@@ -1,21 +1,28 @@
 package com.it.cinemabackend.notification;
 
 import com.it.cinemabackend.auth.domain.model.User;
+import com.it.cinemabackend.auth.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Service @Slf4j
+@Service
+@Slf4j
 public class NotificationService {
 
-    private final JavaMailSender javaMailSender;
-
-    // TEMPORARY SOLUTION
+    private static final String EMAIL_FROM = "default";
+    private static final long ADVERT_DELAY = 1000 * 60 * 60 * 24 * 7; // week
+    // TODO REMOVE THAT TEMPORARY SOLUTION github #60
     private static final String ACTIVATION_ADDRESS = "http://localhost:8080/activate/";
 
-    public NotificationService(JavaMailSender javaMailSender) {
+    private final UserService userService;
+    private final JavaMailSender javaMailSender;
+
+    public NotificationService(UserService userService, JavaMailSender javaMailSender) {
+        this.userService = userService;
         this.javaMailSender = javaMailSender;
     }
 
@@ -28,5 +35,25 @@ public class NotificationService {
 
         javaMailSender.send(mail);
         log.info("Mail has just been sent to: " + user.getEmail());
+    }
+
+    @Scheduled(initialDelay = ADVERT_DELAY, fixedDelay = ADVERT_DELAY)
+    public void sendAdvertEmail() {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setFrom(EMAIL_FROM);
+        mail.setSubject("Activate your account!");
+        mail.setText("Scheduled email is here.");
+
+        for (User user : userService.loadUsers()) {
+            mail.setTo(user.getEmail());
+            try {
+                javaMailSender.send(mail);
+            }
+            catch (MailException e) {
+                log.error("Could not send advert mails due to mail exception: {}", e.toString());
+                return;
+            }
+        }
+        log.info("Sent advertisement email to every user.");
     }
 }
